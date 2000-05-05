879474329ffee38de9b51d1e866d3cdf91e25314
columbus <columbus>
2000-05-05 11:08:56 +0000

    changed algorithm of get_key() to allow AVerMedia support

--- drivers/lirc_gpio/lirc_gpio_p.c
+++ drivers/lirc_gpio/lirc_gpio_p.c
@@ -3,9 +3,10 @@
  * key codes are obtained from GPIO port
  * 
  * (L) by Artur Lipowski <lipowski@comarch.pl>
- *        This code is licensed under GNU GPL
+ *     patch for the AverMedia by Santiago Garcia Mantinan <manty@i.am>
+ * This code is licensed under GNU GPL
  *
- * $Id: lirc_gpio_p.c,v 1.2 2000/04/06 17:23:58 columbus Exp $
+ * $Id: lirc_gpio_p.c,v 1.3 2000/05/05 11:08:56 columbus Exp $
  *
  */
 
@@ -16,58 +17,77 @@
 
 #include <linux/module.h>
 #include <linux/kmod.h>
-/* i2c.h does not include spinlock.h */
-#include <asm/spinlock.h>
+#include <linux/sched.h>
 #include <linux/wrapper.h>
 
 #include "../lirc_dev/lirc_dev.h"
 #include "../drivers/char/bttv.h"
-#include "../drivers/char/bt848.h"
 
-/* default parameters value are suitable for the PixelView Play TVPro card */
+/* default parameters value are suitable for the PixelView Play TVPro card
+ * for the AverMedias you should use:
+ * gpio_lock_mask=0x010000
+ * gpio_xor_mask=0x010000
+ * gpio_mask=0x0f88000
+ */                                                                                                    
 
 static int debug = 0;
 static int card = 0;
 static int minor = -1;
-static int code_length = 5;
-static unsigned int gpio_mask = 0x1f;
-static int gpio_shift = 8;
+static unsigned int gpio_mask = 0x1f00;
 static unsigned int gpio_lock_mask = 0x8000;
-static int soft_gap = 300; /* ms  0 - disable */
-static int sample_rate = 10;
+static unsigned int gpio_xor_mask = 0;
+static unsigned int soft_gap = 500; /* ms  0 - disable */
+static unsigned int sample_rate = 12;
 
 MODULE_PARM(debug,"i");
 MODULE_PARM(card,"i");
 MODULE_PARM(minor,"i");
-MODULE_PARM(code_length,"i");
 MODULE_PARM(gpio_mask,"i");
-MODULE_PARM(gpio_shift,"i");
 MODULE_PARM(gpio_lock_mask,"i");
+MODULE_PARM(gpio_xor_mask,"i");
 MODULE_PARM(soft_gap,"i");
 MODULE_PARM(sample_rate,"i");
 
 #define dprintk  if (debug) printk
 
 
+/* how many bits GPIO value can be shifted right before processing
+ * it is computed from the value of gpio_mask_parameter
+ */
+static unsigned char gpio_pre_shift = 0;
+
 static int get_key(void* data, unsigned char *key, int key_no)
 {
-	unsigned long code;
-	unsigned char curr_key;
-	static unsigned char last_key = 0xff;
 	static unsigned long next_time = 0;
+	static unsigned char last_key = 0xff;
+
+	unsigned long code;
+	unsigned int mask = gpio_mask;
+	unsigned char shift = 0;
+	unsigned char curr_key = 0;
 
 	if (bttv_read_gpio(card, &code)) {
 		dprintk("lirc_gpio_p %d: cannot read GPIO\n", card);
 		return -1;
 	}
 
-	if (gpio_lock_mask && (code & gpio_lock_mask)) {
-		last_key = 0xff;
-		return -1;
+	code ^= gpio_xor_mask;
+
+ 	if (gpio_lock_mask && (code & gpio_lock_mask)) {
+ 		last_key = 0xff;
+ 		return -1;
+ 	}
+
+	/* extract bits from "raw" GPIO value using gpio_mask */
+	code >>= gpio_pre_shift;
+	while (mask) {
+		if (mask & 1u) {
+			curr_key |= (code & 1u) << shift++;
+		}
+		mask >>= 1;
+		code >>= 1;
 	}
-
-	curr_key = (code >> gpio_shift) & gpio_mask;
-
+	   
 	if (curr_key == last_key && jiffies < next_time) {
 		return -1;
 	}
@@ -106,12 +126,30 @@ static struct lirc_plugin plugin = {
 int gpio_remote_init(void)
 {  	
 	int ret;
-	
+	unsigned int mask;
+
+	/* "normalize" gpio_mask
+	 * this means shift it right until first bit is set
+	 */
+	while (!(gpio_mask & 1u)) {
+		gpio_pre_shift++;
+		gpio_mask >>= 1;
+	}
+
+	/* calculte scan code length in bits */
+	plugin.code_length = 1;
+	mask = gpio_mask >> 1;
+	while(mask) {
+		if (mask & 1u) {
+			plugin.code_length++;
+		}
+		mask >>= 1;
+	}
+
 	/* translate ms to jiffies */
 	soft_gap = (soft_gap*HZ) / 1000;
 
 	plugin.minor = minor;
-	plugin.code_length = code_length;
 	plugin.sample_rate = sample_rate;
 
 	ret = lirc_register_plugin(&plugin);
@@ -141,8 +179,6 @@ MODULE_AUTHOR("Artur Lipowski");
  */
 int init_module(void)
 {
-	unsigned int max_mask;
-
 	if (MAX_IRCTL_DEVICES < minor) {
 		printk("lirc_gpio_p: parameter minor (%d) must be lesst han %d!\n",
 		       minor, MAX_IRCTL_DEVICES-1);
@@ -155,26 +191,8 @@ int init_module(void)
 		return -1;
 	}
 
-	if (1 > code_length || 16 < code_length) {
-		printk("lirc_gpio_p %d: parameter code_length (%d) has bad value!\n",
-		       minor, code_length);
-		return -1;
-	}
-
-	max_mask = code_length > 8 ? 0xffff : 0xff;
-	if (gpio_mask) {
-		if (gpio_mask > max_mask) {
-		printk("lirc_gpio_p %d: parameter gpio_mask (%d) must be non zero "
-		       "and not greather than %u!\n", minor, gpio_mask, max_mask);
-		return -1;
-		}
-	} else {
-		gpio_mask = max_mask;
-	}
-
-	if ((24-code_length) < (unsigned)gpio_shift ) {
-		printk("lirc_gpio_p %d: parameter gpio_shift must be less than %d!\n",
-		       minor, 24-code_length);
+	if (!gpio_mask) {
+		printk("lirc_gpio_p %d: parameter gpio_mask cannot be zero!\n", minor);
 		return -1;
 	}
 
@@ -184,10 +202,7 @@ int init_module(void)
 		return -1;
 	}
 
-	if (!soft_gap) {
-		/* do not allow enter the new code more often than sample_rate*2 */
-		soft_gap = 2000 / sample_rate;
-	} else if ((2000/sample_rate) > soft_gap || 1000 < soft_gap) {
+	if (soft_gap && ((2000/sample_rate) > soft_gap || 1000 < soft_gap)) {
 		printk("lirc_gpio_p %d: parameter soft_gap must be beetween %d and 1000!\n",
                        minor, 2000/sample_rate);
 		return -1;
