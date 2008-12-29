29d03ed7c3794e809ee3e52fa9679442e7bf9a3a
lirc <lirc>
2008-12-29 16:55:33 +0000

    handle comments

--- daemons/input_map.sh
+++ daemons/input_map.sh
@@ -4,6 +4,6 @@ TYPES="KEY BTN"
 file=${1:-/usr/include/linux/input.h}
 
 for type in $TYPES; do
-	grep "^#define ${type}_" < $file|sort|sed --expression="s/^#define \([^ 	]*\)\(.*\)/{\"\1\", \2},/"|grep -v "KEY_HANGUEL.*KEY_HANGEUL\|KEY_MIN_INTERESTING"
+	grep "^#define ${type}_" < $file|sort|sed -n --expression="s/^#define \([^ 	]*\)[ 	][ 	]*\([0-9][0-9a-fA-FxX]*\).*/{\"\1\", \2},/p"
 done
 
