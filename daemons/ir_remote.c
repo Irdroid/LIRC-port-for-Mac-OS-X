9592bb4a2bccedf5ef205cf93c4d17f5b27edf5c
Jarod Wilson <jarod@redhat.com>
2011-06-29 17:31:28 -0400

    Silence some clang warnings, courtesy of nox
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- daemons/ir_remote.c
+++ daemons/ir_remote.c
@@ -490,7 +490,8 @@ int write_message(char *buffer, size_t size, const char *remote_name, const char
 {
 	int len;
 
-	len = snprintf(buffer, size, "%016llx %02x %s%s %s\n", code, reps, button_name, button_suffix, remote_name);
+	len = snprintf(buffer, size, "%016llx %02x %s%s %s\n",
+		(unsigned long long)code, reps, button_name, button_suffix, remote_name);
 
 	return len;
 }
